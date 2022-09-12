2919aabbad8a93e6876d1ca4967c199298d321a6
Deepika2605 <44749291+Deepika2605@users.noreply.github.com>
2022-09-12 17:42:53 +0200

    updating dev

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {makeStyles} from "@material-ui/core/styles"
-import { Typography, Grid, Avatar, Box, BottomNavigation } from "@material-ui/core";
+import { Typography, Grid, Avatar, Box } from "@material-ui/core";
 import Typed from "react-typed";
 import avatar from "../profilePhoto.jpg";
 
@@ -16,16 +16,18 @@ const useStyles = makeStyles(theme=> ({
     },
     subtitle: {
         color: "#27496d",
-        marginBottom: "3rem"
     },
     typedContainer: {
         position: "absolute",
-        top: "50%",
+        top: "45%",
         left: "50%",
         transform: "translate(-50%, -50%)",
-        width: "100vw",
+        width: "100%",
         textAlign: "center",
         zIndex: 1
+    },
+    footerText: {
+        marginBottom: ""
     }
 }))
 const Header = () => {
@@ -42,14 +44,13 @@ const Header = () => {
             </Typography>
             <br/>
             <Typography className={classes.subtitle} variant="h5">
-                <Typed strings={["Web Designer", "Software Developer", "UI/UX Designer"]} 
+                <Typed strings={["Web Designer", "Frontend Developer", "UI/UX Designer"]} 
                 typeSpeed={40}
                 backSpeed={60}
                 loop/>
             </Typography>
-            <BottomNavigation width="auto" style={{background: "transparent"}} >
-                <Typography className={classes.subtitle} variant="caption">Developed By: Deepika Sathianarayanan</Typography>
-            </BottomNavigation>
+            <br/>
+            <Typography className="description">I'm a person with skills of a developer and a mindset of a designer <br/> which can help to fill in gaps between the design and its fully functional product <br/> inturn providing the best user experience</Typography>
         </Box>
         </>
     )
